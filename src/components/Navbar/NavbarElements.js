c96d5cc862d0392b66f4aec56bf10738ca31a8ed
Carlos Chapa <bravelittletodolist@gmail.com>
2020-11-23 12:47:13 -0700

    added sidebar

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -35,7 +35,7 @@ export const NavIcon = styled.div`
 
 
     p{
-        transform: translate(-175%, 100%);
+        transform: translate(-175%, 125%);
         font-weight: bold;
     }
 `;
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,18 +1,18 @@
-import React from 'react'
-import { Nav, NavLink, NavIcon, Bars } from './NavbarElements'
+import React from 'react';
+import { Nav, NavLink, NavIcon, Bars } from './NavbarElements';
 
-const Navbar = () => {
-    return (
-        <>
-            <Nav>
-                <NavLink to='/'>BL Pizza</NavLink>
-                <NavIcon>
-                    <p>Menu</p>
-                    <Bars />
-                </NavIcon>
-            </Nav>
-        </>
-    )
-}
+const Navbar = ({ toggle }) => {
+  return (
+    <>
+      <Nav>
+        <NavLink to='/'>BL Pizza</NavLink>
+        <NavIcon onClick={toggle}>
+          <p>Menu</p>
+          <Bars />
+        </NavIcon>
+      </Nav>
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
